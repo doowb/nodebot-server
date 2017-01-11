18181ab11f6ff72b9a18d0d758232b608e0ece65
doowb <brian.woodward@gmail.com>
2017-01-11 13:24:01 -0500

    allow configuration of cwd

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 var path = require('path');
 var server = require('./');
-server(path.join(__dirname, 'index.html'), function(err, app) {
+server({cwd: path.join(__dirname), index: 'index.html'}, function(err, app) {
   app.on('cmd', function(cmd) {
     console.log('emitted cmd:', cmd);
   });
--- server.js
+++ server.js
@@ -2,14 +2,19 @@
 
 var path = require('path');
 var express = require('express');
+var extend = require('extend-shallow');
 
-module.exports = function(fp) {
+module.exports = function(config) {
+  config = extend({
+    cwd: process.cwd(),
+    index: 'index.html'
+  }, config);
+
   var app = express();
-
-  app.use(express.static(process.cwd()));
+  app.use(express.static(config.cwd));
 
   app.get('/', function(req, res){
-    res.sendFile(fp);
+    res.sendFile(path.join(config.cwd, config.index));
   });
 
   return app;
